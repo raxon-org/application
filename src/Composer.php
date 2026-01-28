553f1787471d88ceefb09939913c31aa89f6f68a
Remco van der Velde <remco@universeorange.com>
2026-01-28 20:21:42 +0100

    init

--- src/Composer.php
+++ src/Composer.php
@@ -6,16 +6,9 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'Debug.php';
 class Composer
 {
 
-    public static function binary()
-    {
-        touch('/usr/bin/microstorm');
-    }
-
     public static function update(): void
     {
         echo 'Updating application, please wait...' . PHP_EOL;
-        echo 'Creating binary...' . PHP_EOL;
-        Composer::binary();
     }
 
 }
