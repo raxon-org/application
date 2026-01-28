1e0af55fe0028473ce00c351e4f8f38e01f36c82
Remco van der Velde <remco@universeorange.com>
2026-01-28 20:04:32 +0100

    init

--- src/Composer.php
+++ src/Composer.php
@@ -10,6 +10,10 @@ class Composer
     public static function update(): void
     {
         echo 'Updating application, please wait...' . PHP_EOL;
+        echo 'Starting Frankenphp' . PHP_EOL;
+        exec("frankenphp run --config /Application/Caddyfile &", $output, $code);
+        echo 'Frankenphp exited with code ' . $code . PHP_EOL;
+        d($output);
         if(!File::exist('/usr/bin/microstorm')){
             echo 'Creating binary...' . PHP_EOL;
             ddd('where is the binary create?');
