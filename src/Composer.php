ff199fb1aed992264f51d1231a358d939cac8ea7
Remco van der Velde <remco@universeorange.com>
2026-01-28 19:57:16 +0100

    init

--- src/Composer.php
+++ src/Composer.php
@@ -5,8 +5,13 @@ namespace Application;
 class Composer
 {
 
-    public static function update(){
+    public static function update(): void
+    {
         echo 'Updating application, please wait...' . PHP_EOL;
+        if(!File::exist('/usr/bin/microstorm')){
+            echo 'Creating binary...' . PHP_EOL;
+            ddd('where is the binary create?');
+        }
     }
 
 }
