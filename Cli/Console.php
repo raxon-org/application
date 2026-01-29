f53ec883976e81a2d30b08edddf6f18a629589ed
Remco van der Velde <remco@universeorange.com>
2026-01-29 12:03:04 +0100

    k

--- Cli/Console.php
+++ Cli/Console.php
@@ -1,5 +1,6 @@
 <?php
 define('MICROSTORM', microtime(true));
+define('IS_CLI', true);
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = require dirname(__DIR__) . '/src/Application.php';
 echo $app->run($_SERVER, $_FILES, $_COOKIE);
