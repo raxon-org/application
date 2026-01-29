55c49ff35ac3659d1e449642b04f5ff63042a408
Remco van der Velde <remco@universeorange.com>
2026-01-29 11:59:48 +0100

    k

--- Cli/Console.php
+++ Cli/Console.php
@@ -2,6 +2,5 @@
 define('MICROSTORM', microtime(true));
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = require dirname(__DIR__) . '/src/Application.php';
-echo 'Happy Console...' . PHP_EOL;
-d($app);
+echo $app->run($_SERVER, $_FILES, $_COOKIE);
 //ignore_user_abort(true);
