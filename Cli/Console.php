7859ac6e2d37b14eaff109d496371f674d6cf9ae
Remco van der Velde <remco@universeorange.com>
2026-01-29 11:55:45 +0100

    init

--- Cli/Console.php
+++ Cli/Console.php
@@ -1,3 +1,11 @@
 <?php
-
+require_once dirname(__DIR__) .
+    DIRECTORY_SEPARATOR .
+    '..' .
+    DIRECTORY_SEPARATOR .
+    'vendor/autoload.php';
+//ignore_user_abort(true);
+define('MICROSTORM', microtime(true));
+$app = require dirname(__DIR__) . '/src/Application.php';
 echo 'Happy Console...' . PHP_EOL;
+d($app);
